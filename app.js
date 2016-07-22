a2e5786d81e6f92d3c2660940123820b3b3cecca
ma2ta2 <yushiro.ma2ta2.21@gmail.com>
2016-07-22 17:50:08 +0900

    naosita

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 var mongoose = require('mongoose');
-var app = express();
-var post = require('./routes/post');
+  app = express();
+  post = require('./routes/post');
 var path = require('path');
 
 
@@ -12,21 +12,7 @@ var path = require('path');
   var Schema = mongoose.Schema;
 
 
-      // ToDoスキーマを定義する
-      var todoSchema = new Schema({
-        isCheck: {type: Boolean, default: false},
-        text: String,
-        createdDate: {type: Date, default: Date.now},
-        limitDate: Date
-      });
 
-
-      var listSchema = new Schema({
-        title: String,
-        todos: [todoSchema],
-        todoValues: []
-      });
-      mongoose.model('List', listSchema);
 
 
 
@@ -48,54 +34,30 @@ var path = require('path');
   mongoose.model('List', listSchema);
 
 
-    var List = mongoose.model('List');
-    List.findOne({_id: '5787e598061ffc168c34c10d'}, function (err, list) {
-      if (!err) {
-      var todoValues = [];
-      list.forEach(function (list) {
-        var todoCount = 0;
-        var todoLength = list.todos.length;
-        list.todos.forEach(function (todo) {
-          if (todo.isCheck) {
-            todoCount++;
-          }
-          todoValues.push({
-            todoCount: todoCount,
-            todoLength: todoLength
-          });
-        });
-      });
-      }
-      else {
-        console.log('あ');
-      }
-});
+  //
+  // Todo.findOne({ _id: post_id }, function(err, post) {
+  //   if (!err) {
+  //     var todos = {};
+  //     todos.isCheck = false;
+  //     todos.text  = '内容';
+  //     todos.createdDate  = Date.now();
+  //     todos.todos.push(todos);  // 追加
+  //     post.save(function(err) {
+  //       // ...
+  //     });
+  //   } else {
+  //     console.log('error findOne: ' + err);
+  //   }
+  // });
 
 
 
-      //
-      // Todo.findOne({ _id: post_id }, function(err, post) {
-      //   if (!err) {
-      //     var todos = {};
-      //     todos.isCheck = false;
-      //     todo.text  = '内容';
-      //     todos.createdDate  = Date.now();
-      //     todos.todos.push(todos);  // 追加
-      //     post.save(function(err) {
-      //       // ...
-      //     });
-      //   } else {
-      //     console.log('error findOne: ' + err);
-      //   }
-      // });
 
-
-      app.set('views', __dirname + '/views');
-      app.set('view engine', 'ejs');
+  app.set('views',__dirname + '/views');
+  app.set('view engine', 'ejs');
 
 
 //middleware
-
 
   app.use(bodyParser.json());
   app.use(bodyParser.urlencoded({extended: true}));
@@ -109,19 +71,17 @@ var path = require('path');
   }));
   app.use(express.static(path.join(__dirname, 'public')));
 
-
-      mongoose.connect('mongodb://localhost/local');
+  mongoose.connect('mongodb://localhost/local');
 
 //routing
 
 // /todoにGETアクセスしたとき、ToDo一覧を取得するAPI
-
 app.get('/todo', function(req, res) {
   var Todo = mongoose.model('Todo');
   // すべてのToDoを取得して送る
   Todo.find({}, function(err, todos) {
     res.send(todos);
-  });v
+  });
 });
 
 // /todoにPOSTアクセスしたとき、ToDoを追加するAPI
@@ -221,33 +181,9 @@ app.get('/getTodoHtmlPage/:listname',function(req,res) {
 
 
 
-        });
-      });
-
-// /listにPOSTアクセスしたとき、Listを追加するAPI
-      app.post('/list', function (req, res) {
-        var title = req.body.title;
-        // ToDoの名前と期限のパラーメタがあればMongoDBに保存
-        if (title) {
-          var List = mongoose.model('List');
-          var list = new List();
-          list.title = title;
-          list.save();
-
-          res.send(true);
-        } else {
-          res.send(false);
-        }
-      });
-
-      app.get('/list/:id', function (req, res) {
-        var List = mongoose.model('List');
-        // すべてのlistを取得して送る
-        List.findById(req.params.id, function (err, lists) {
-          res.send(lists);
-        });
-      });
 
 
-      app.listen(3000);
-      console.log('server listening...');
+
+
+  app.listen(3000);
+  console.log('server listening...');
