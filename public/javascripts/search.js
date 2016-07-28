9200a0cd3f0d5bb720067e76ea893cfcbb18e97d
ma2ta2 <yushiro.ma2ta2.21@gmail.com>
2016-07-29 04:30:08 +0900

    検索で対象のtodoがなかった場合の処理追加

--- public/javascripts/search.js
+++ public/javascripts/search.js
@@ -6,22 +6,29 @@ $button.click(function () {
         $.get('/goSearch/' + $('#textArea').val()).done(function (data) {
             console.log(data);
             var node = document.getElementById('fuga');
-            data.forEach(function (d) {
-                var elementText = document.createElement('li');
-                    elementText.innerHTML = "<a href=/getTodoHtmlPage/"+d.listname+">"+d.text+"</a>"
-                var elementListname = document.createElement('li');
-                    elementListname.innerHTML = "リスト:"+d.listname;
-                var elementLimit = document.createElement('li');
-                    elementLimit.innerHTML = "期限:"+d.limitDate;
-                var elementCreate = document.createElement('li');
-                    elementCreate.innerHTML = "作成日時"+d.createdDate;
+                if(data.length == 0){
+                    var elementError = document.createElement('li');
+                    elementError.innerHTML = "対象のTodoは見つかりません";
+                    node.appendChild(elementError);
+                } else {
 
-                node.appendChild(elementText);
-                node.appendChild(elementListname);
-                node.appendChild(elementLimit);
-                node.appendChild(elementCreate);
-            });
-            $li.fadeIn();
+                    data.forEach(function (d) {
+                        var elementText = document.createElement('li');
+                        elementText.innerHTML = "<a href=/getTodoHtmlPage/" + d.listname + ">" + d.text + "</a>"
+                        var elementListname = document.createElement('li');
+                        elementListname.innerHTML = "リスト:" + d.listname;
+                        var elementLimit = document.createElement('li');
+                        elementLimit.innerHTML = "期限:" + d.limitDate;
+                        var elementCreate = document.createElement('li');
+                        elementCreate.innerHTML = "作成日時" + d.createdDate;
+
+                        node.appendChild(elementText);
+                        node.appendChild(elementListname);
+                        node.appendChild(elementLimit);
+                        node.appendChild(elementCreate);
+                    });
+                    $li.fadeIn();
+                }
         });
 
     });
